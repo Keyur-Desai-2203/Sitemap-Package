853924f490fd52cdba0dd75a55b9452c43acf3b8
Keyur Desai <keyurdtridhyatech@gmail.com>
2023-12-15 15:37:40 +0530

    Sitemap generate by url and other parameters added in xml file.

--- src/SitemapServiceProvider.php
+++ src/SitemapServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Keyur\Sitemap;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 use Keyur\Sitemap\Console\Commands\GenerateSitemap;
 class SitemapServiceProvider extends ServiceProvider
 {
@@ -10,14 +12,25 @@ class SitemapServiceProvider extends ServiceProvider
     public function boot()
     {
         // Publishes the configuration file if needed
-        $this->publishes([
-            __DIR__.'/config/sitemap.php' => config_path('sitemap.php'),
-        ], 'config');
+        // $this->publishes([
+        //     __DIR__.'/config/sitemap.php' => config_path('sitemap.php'),
+        // ], 'config');
+
+        // if ($this->app->runningInConsole()) {
+        //     $this->commands([
+        //         GenerateSitemap::class,
+        //     ]);
+        // }
 
         if ($this->app->runningInConsole()) {
             $this->commands([
                 GenerateSitemap::class,
             ]);
+        } else {
+            Route::get('/sitemap', function () {
+                Artisan::call('sitemap:generate');
+                return response()->json(['message' => 'Sitemap generated successfully']);
+            });
         }
     }
 
--- src/console/commands/GenerateSitemap.php
+++ src/console/commands/GenerateSitemap.php
@@ -48,8 +48,17 @@ class GenerateSitemap extends Command
         $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
         foreach ($urls as $url) {
+
+            $lastModified = $this->generateLastModified(); // Replace this with your logic to generate last modified date
+        $changeFrequency = $this->generateChangeFrequency(); // Replace this with your logic to generate change frequency
+        $priority = $this->generatePriority(); // Replace this with your logic to generate priority
+
+
             $xml .= '<url>';
             $xml .= '<loc>' . htmlspecialchars($url) . '</loc>';
+            $xml .= '<lastmod>' . $lastModified . '</lastmod>';
+            $xml .= '<changefreq>' . $changeFrequency . '</changefreq>';
+            $xml .= '<priority>' . $priority . '</priority>';
             $xml .= '</url>';
         }
 
@@ -57,4 +66,31 @@ class GenerateSitemap extends Command
 
         return $xml;
     }
+    private function generateLastModified()
+{
+    // Replace this with your logic to generate last modified date dynamically
+    // For example, retrieving the last modified date from your database for each URL
+    // This is a placeholder; replace it with your actual logic
+    // return YourModel::where('url', $url)->latest('updated_at')->value('updated_at');
+    return date('Y-m-d');
+}
+
+private function generateChangeFrequency()
+{
+    // Replace this with your logic to generate change frequency dynamically
+    // This is a placeholder; you might determine change frequency based on the URL type or other factors
+    // Example logic:
+    $changeFrequencies = ['always', 'hourly', 'daily', 'weekly', 'monthly', 'yearly', 'never'];
+    return $changeFrequencies[rand(0, count($changeFrequencies) - 1)]; // Randomly select a change frequency
+}
+
+private function generatePriority()
+{
+    // Replace this with your logic to generate priority dynamically
+    // This is a placeholder; you might determine priority based on the importance of the URL
+    // Example logic:
+    $priorityValues = ['0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1.0'];
+    return $priorityValues[rand(0, count($priorityValues) - 1)]; // Randomly select a priority value
+}
+
 }
